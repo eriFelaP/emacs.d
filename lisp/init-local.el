c193dc8118ff5f0841ce3ea4a859640cdd04113c
ZongnanHu <zongnan_hu@163.com>
2020-04-30 22:27:07 +0800

    Update

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -287,12 +287,52 @@ http://ergoemacs.org/emacs/emacs_copy_file_path.html"
 (setq deft-directory znh/note-dir)
 
 
-(require-package 'paredit)
+(require 'paredit)
 
 (define-key paredit-mode-map (kbd "C-c )") 'paredit-forward-slurp-sexp)
 (define-key paredit-mode-map (kbd "C-c (") 'paredit-forward-barf-sexp)
 
+;; http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html
+(defun xah-open-in-external-app (&optional @fname)
+  "Open the current file or dired marked files in external app.
+The app is chosen from your OS's preference.
+
+When called in emacs lisp, if @fname is given, open that.
+
+URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
+Version 2019-11-04"
+  (interactive)
+  (let* (
+         ($file-list
+          (if @fname
+              (progn (list @fname))
+            (if (string-equal major-mode "dired-mode")
+                (dired-get-marked-files)
+              (list (buffer-file-name)))))
+         ($do-it-p (if (<= (length $file-list) 5)
+                       t
+                     (y-or-n-p "Open more than 5 files? "))))
+    (when $do-it-p
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc
+         (lambda ($fpath)
+           (w32-shell-execute "open" $fpath)) $file-list))
+       ((string-equal system-type "darwin")
+        (mapc
+         (lambda ($fpath)
+           (shell-command
+            (concat "open " (shell-quote-argument $fpath))))  $file-list))
+       ((string-equal system-type "gnu/linux")
+        (mapc
+         (lambda ($fpath) (let ((process-connection-type nil))
+                        (start-process "" nil "xdg-open" $fpath))) $file-list))))))
+
+(global-set-key [remap whole-line-or-region-kill-ring-save] 'easy-kill)
+
 (provide 'init-local)
+
+
 
 ;; Local Variables:
 ;; coding: utf-8
