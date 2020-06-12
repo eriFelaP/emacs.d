65f3e1be699ca59acb7c18b7160d57f10ce5cb44
ZongnanHu <zongnan_hu@163.com>
2020-06-12 20:13:35 +0800

    Update

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -12,21 +12,19 @@
 ;; If you don't customize it, this is the theme you get.
 (setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
 
+(require-package 'spaceline)
+(require 'spaceline-config)
+
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
   "Forcibly load the themes listed in `custom-enabled-themes'."
   (dolist (theme custom-enabled-themes)
     (unless (custom-theme-p theme)
       (load-theme theme)))
-  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes)))
+  (spaceline-emacs-theme))
 
 (add-hook 'after-init-hook 'reapply-themes)
-
-
-(require-package 'spaceline)
-(require 'spaceline-config)
-(spaceline-emacs-theme)
-
 
 ;;------------------------------------------------------------------------------
 ;; Toggle between light and dark
@@ -35,15 +33,13 @@
   "Activate a light color theme."
   (interactive)
   (setq custom-enabled-themes '(sanityinc-tomorrow-day))
-  (reapply-themes)
-  (spaceline-emacs-theme))
+  (reapply-themes))
 
 (defun dark ()
   "Activate a dark color theme."
   (interactive)
   (setq custom-enabled-themes '(sanityinc-tomorrow-bright))
-  (reapply-themes)
-  (spaceline-emacs-theme))
+  (reapply-themes))
 
 
 (when (maybe-require-package 'dimmer)
